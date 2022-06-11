c2d75384eddb1a0f8b3fcfbb2a2d01e369d5b876
lawrence <prostoLawr@gmail.com>
2022-06-11 20:53:07 +0800

    Add files

---
+++ script.js
@@ -0,0 +1,7 @@
+function xyi() {
+    var text_input=document.getElementById("text_input");
+    var text_output=document.getElementById("text");
+    var name=text_input.value;
+    var text= "пошел нахуй, " + name;
+    text_output.innerHTML=text;
+}
